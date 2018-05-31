6ea22f1c43ac7166e0546fa963c8306a7cd26e64
pieter bracke <pieterbracke@msn.com>
2018-05-31 20:04:43 +0200

    Added User controller and login possibility Added Admin controller Added complentary Views

--- Gamification/Controllers/HomeController.cs
+++ Gamification/Controllers/HomeController.cs
@@ -3,14 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Gamification.Models;
+using Gamification.Repositorys;
 
 namespace Gamification.Controllers
 {
     public class HomeController : Controller
     {
+        private UserRepository _userRepository = null;
+
+        public HomeController()
+        {
+            _userRepository = new UserRepository();
+        }
+
         public ActionResult Index()
         {
+            _userRepository.LoadAdmins();
             return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("User");
+            return RedirectToAction("Index", "Home", null);
+        }
+
+        [HttpPost]
+        public ActionResult Bevestiging(string username, string password)
+        {
+            var user = _userRepository.GetUser(username, password);
+            if (user != null)
+            {
+                Session["User"] = user;
+
+                TempData["LoginValid"] = "Welcome " + user.First_Name + " " + user.Last_Name;
+                return View(user);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Login data was not correct please try again";
+                return RedirectToAction("Login", "Home");
+            }
+
         }
 
         public ActionResult About()
---
+++ Gamification/Repositorys/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gamification.Models;
+
+namespace Gamification.Repositorys
+{
+    public class UserRepository
+    {
+        private GamificationEntities db = new GamificationEntities();
+
+        //Load the 2 admin accounts in the database
+        public void LoadAdmins()
+        {
+            var query = (from user in db.Users where user.Username == "Admin1" select user).ToList();
+            if (query.Count < 1)
+            {
+                db.Users.Add(
+                    new Users
+                    {
+                        
+                        Username = "Admin1",
+                        Email = "Admin1@admin.com",
+                        First_Name = "Admin1",
+                        Last_Name = "Admin1",
+                        Role = "Admin",
+                        Country = "BE",
+                        Division = "Admin",
+                        Password = "BrackeAdmin1993",
+
+                    });
+
+                db.Users.Add(
+                    new Users
+                    {
+
+                        Username = "Admin2",
+                        Email = "Admin2@admin.com",
+                        First_Name = "Admin2",
+                        Last_Name = "Admin2",
+                        Role = "Admin",
+                        Country = "BE",
+                        Division = "Admin",
+                        Password = "BrackeAdmin1993",
+
+                    });
+
+                db.SaveChanges();
+            }
+        }
+
+        public List<Users> GetUsers()
+        {
+            return db.Users.OrderBy(u => u.UserID).ToList();
+        }
+
+        public Users GetUser(string username, string password)
+        {
+            var User = (from user in db.Users where user.Username == username && user.Password == password select user).FirstOrDefault();
+            return User;
+        }
+    }
+}
